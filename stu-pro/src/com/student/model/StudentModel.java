35376fc3112e122a9e35906f0ab3fcb5e40d4bf6
Ruandan2020 <574381216@qq.com>
2020-06-09 17:18:01 +0800

    push my eclipse to git

---
+++ stu-pro/src/com/student/model/StudentModel.java
@@ -0,0 +1,27 @@
+package com.student.model;
+
+import java.util.List;
+
+public class StudentModel {
+
+	public void addUser(int id,String username,String password) {
+		
+		//add
+		
+	}
+			public void updateUser(int id,String newUsername,String newPassword) {
+					
+					//update
+					
+				}
+		public void deleteUser(int id) {
+			
+			//add
+			
+		}
+    public List<Object> scanUser(int pageIndex,int pageSize){
+		return null;
+    	
+    	
+    }
+}
