9234d24fa91f46906ca3e9dc30a62a7530b57f02
Ruandan2020 <574381216@qq.com>
2020-06-23 10:44:19 +0800

    xiugao

--- stu-pro/src/com/student/model/StudentModel.java
+++ stu-pro/src/com/student/model/StudentModel.java
@@ -7,11 +7,13 @@ public class StudentModel {
 	public void addUser(int id,String username,String password) {
 		
 		//add
+		String  name1;
 		
 	}
 			public void updateUser(int id,String newUsername,String newPassword) {
 					
 					//update
+				
 					
 				}
 		public void deleteUser(int id) {
